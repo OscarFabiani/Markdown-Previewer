d6a21cccb3a0a6188d97d768b9307e8c3b14a985
OscarFabiani <spartan539@yahoo.com>
2019-02-21 11:25:07 -0800

    first draft of project

---
+++ script.js
@@ -0,0 +1,62 @@
+const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
+
+editor.addEventListener('input', updateText);
+
+function updateText() {
+  let newText = editor.value;
+  preview.innerHTML = marked(newText, breaks = true);
+}
+
+window.addEventListener('load', updateText);
+
+let defaultText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)`
+
+editor.value = defaultText;
+
+//CREATE GITHUB REPOSITORY AND REPLICATE COLOR SCHEME, DROP SHADOW, SCROLLABILITY, EXPAND/CONTRACT BUTTON, MINIMUM HEIGHT, HORIZONTAL RESPONSIVITY, STYLING,
+//AND POSSIBLY SOLVE LINEBREAK ISSUE.
